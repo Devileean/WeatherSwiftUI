4a5f20e0e9e4e881d149b0ec8ea4f35e428e4827
Alexsey Loginov <aleniklog@gmail.com>
2022-08-15 21:56:11 +0300

    added git

---
+++ WeatherShiftUI/WeatherShiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherShiftUIApp.swift
+//  WeatherShiftUI
+//
+//  Created by Алексей Логинов on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherShiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
