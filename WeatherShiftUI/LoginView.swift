ad9ce3805090f22545dfa706492bb873ff81ac4b
Alexsey Loginov <aleniklog@gmail.com>
2022-08-18 01:04:02 +0300

    fix and restucture

--- WeatherShiftUI/LoginView.swift
+++ WeatherShiftUI/LoginView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 import Combine
 
 struct LoginView: View {
-    //MARK: @State
+    
+//MARK: @State
     
     @State private var login = ""
     @State private var password = ""
     @State private var shouldShowLogo: Bool = true
     
-    //MARK: Keyboard
+//MARK: Keyboard
     private let keyboardIsOnPublisher = Publishers.Merge(
         NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
             .map { _ in true },
@@ -24,73 +25,55 @@ struct LoginView: View {
     )
         .removeDuplicates()
     
-    //MARK: body
+//MARK: body
     
     var body: some View {
         
         //MARK: Background
         ZStack {
-            GeometryReader { geometry in Image("weather")
-                    .resizable()
-                    .edgesIgnoringSafeArea(.all)
-                    //.aspectRatio(contentMode: .fill)
-                    //.frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
-                
-            }
+                imageBackround
             
-        
-        //MARK: ScrollView
-        
-        ScrollView(showsIndicators: false) {
-            VStack{
-                if shouldShowLogo {
-                    Text("Weather")
-                        .foregroundColor(Color.yellow)
-                        .font(.largeTitle)
-                        .padding(.top, 40)
-                    
-                    VStack{
-                        HStack{
-                            Text("Login :")
-                                .foregroundColor(Color.yellow)
-                            Spacer()
-                            TextField("Enter your login", text: $login)
-                                .frame(width: 200)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                        }
-                        HStack{
-                            Text("Password :")
-                                .foregroundColor(Color.yellow)
-                            Spacer()
-                            SecureField("Enter passward", text: $password)
-                                .frame(width: 200)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                        }
+//MARK: ScrollView
+            
+            ScrollView(showsIndicators: false) {
+                VStack{
+                    if shouldShowLogo {
+                        Text("Weather")
+                            .foregroundColor(Color.yellow)
+                            .font(.largeTitle)
+                            .padding(.top, 40)
                         
-                    }.frame(maxWidth: 300)
+                        VStack{
+                            HStack{
+                                loginInput
+                            }
+                            HStack{
+                                passwordInput
+                            }
+                            
+                        }.frame(maxWidth: 300)
+                            .padding(.top, 50)
+                        Spacer()
+                        //MARK: Button LogIn
+                        
+                        Button(action: { print("Hello") }) {
+                            Text("Log in")
+                                .frame(width: 240, height: 25)
+                                .foregroundColor(.black)
+                                .background(.yellow)
+                                .cornerRadius (25)
+                            
+                        }
                         .padding(.top, 50)
-                    Spacer()
-                    //MARK: Button LogIn
-                    
-                    Button(action: { print("Hello") }) {
-                        Text("Log in")
-                            .frame(width: 240, height: 25)
-                            .foregroundColor(.black)
-                            .background(.yellow)
-                            .cornerRadius (25)
-                        
+                        .padding(.bottom, 20)
+                        .disabled(login.isEmpty || password.isEmpty)// кнопка не работает, если поля пустые
                     }
-                    .padding(.top, 50)
-                    .padding(.bottom, 20)
-                    .disabled(login.isEmpty || password.isEmpty)// кнопка не работает, если поля пустые
+                }
+                
+                .onReceive(keyboardIsOnPublisher) { isKeyboardOn in withAnimation(Animation.default){
+                    }
                 }
             }
-            
-            .onReceive(keyboardIsOnPublisher) { isKeyboardOn in withAnimation(Animation.easeInOut(duration: 0.5)) {
-                self.shouldShowLogo = !isKeyboardOn
-            }
-            }
-        }
         }
         .accentColor(.red) // цвет начала печати
         .onTapGesture {
@@ -98,15 +81,64 @@ struct LoginView: View {
         }
     }
 }
-/////////////////////////
+
+//MARK: ContentView_Previews
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
     }
 }
-////////////////////////
+
+//MARK: UIApplication
+
 extension UIApplication {
     func endEditing() {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+//MARK: backround login view
+
+private extension LoginView {
+    var imageBackround: some View  {
+        ZStack{
+            GeometryReader { geometry in Image("weather")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
+            }
+        }
+    }
+}
+
+//MARK: Login and Password
+
+private extension LoginView {
+    var loginInput: some View {
+        HStack{
+            Text("Login :")
+                .foregroundColor(Color.yellow)
+            Spacer()
+            TextField("Enter your login", text: $login)
+                .frame(width: 200)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(15)
+        }
+    }
+}
+
+private extension LoginView {
+    var passwordInput: some View {
+        HStack{
+            Text("Password :")
+                .foregroundColor(Color.yellow)
+            Spacer()
+            SecureField("Enter passward", text: $password)
+                .frame(width: 200)
+                .textFieldStyle(RoundedBorderTextFieldStyle().self)
+                .cornerRadius(15)
+        }
     }
 }
